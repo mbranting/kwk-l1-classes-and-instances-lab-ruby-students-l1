89b0d5a436aa5c263c49c293f11c2a6bcd4d75fc
mbranting <mckenna.branting@gmail.com>
2018-06-07 21:09:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
 # dog.rb
+class Dog
+
+attr_accessor :name
+
+def initialize(name)
+    @name=name
+  end
+end
+dog1 = Dog.new("fido")
+dog2 = Dog.new("snoopy")
+dog3 = Dog.new("lassie")
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
 # person.rb
+class Person
+  attr_accessor :name
+  
+  def initializer(name)
+    
+    @name = name
+  end
+end
+person1 = Person.new("adele_goldberg")
+person2 = Person.new("alan_kay")
